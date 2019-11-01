67880b68d5c3db8610ffbb84e372f5b322754912
brittani <britt.sentiere@gmail.com>
2019-11-01 15:49:55 -0500

    lesson edits

--- src/views/MessagesIndex.vue
+++ src/views/MessagesIndex.vue
@@ -26,6 +26,7 @@
 
 <script>
 import axios from "axios";
+import ActionCable from "actioncable";
 
 export default {
   data: function() {
@@ -38,7 +39,25 @@ export default {
     axios.get("/api/messages").then(response => {
       this.messages = response.data;
     });
+  var cable = ActionCable.createConsumer("ws://localhost:3000/cable");
+
+  cable.subscriptions.create("MessagesChannel", {
+        connected: () => {
+          // Called when the subscription is ready for use on the server
+          console.log("Connected to MessagesChannel");
+        },
+        disconnected: () => {
+          // Called when the subscription has been terminated by the server
+          console.log("Call has ended")
+        },
+        received: data => {
+          // Called when there's incoming data on the websocket for this channel
+          console.log("Data from MessagesChannel:", data);
+          this.messages.unshift(data); // update the messages in real time
+        }
+      });
   },
+
   methods: {
     createMessage: function() {
       var params = {
